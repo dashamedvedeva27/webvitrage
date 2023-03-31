aa8ed83005db2b7c15026ac433db6eb78f5f1c66
dashamedvedeva27 <113890805+dashamedvedeva27@users.noreply.github.com>
2023-03-31 10:43:58 +0300

    Add files via upload

---
+++ js/script1.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  $(".logotip").click(function() {
+    $(".logotip").addClass("none");
+    $(".screen2").removeClass("none");
+
+  })
+})
+
+$(document).ready(function(){
+  $(".window").click(function() {
+    $(".logotip").addClass("none");
+    $(".screen2").addClass("none");
+    $(".screen3").removeClass("none");
+  })
+})
+
+  $( function() {
+    $( "#draggable1" ).draggable({ snap: ".ui-widget-header", snapMode: "inner" });
+    $( "#draggable2" ).draggable({ snap: ".ui-widget-header", snapMode: "inner" });
+    $( "#draggable3" ).draggable({ snap: ".ui-widget-header", snapMode: "inner" });
+    $( "#draggable4" ).draggable({ snap: ".ui-widget-header", snapMode: "inner" });
+    $( "#draggable5" ).draggable({ snap: ".ui-widget-header", snapMode: "inner" });
+    $( "#draggable6" ).draggable({ snap: ".ui-widget-header", snapMode: "inner" });
+     } );
+
+      const arr = ["#EE4629", "#364CA2" , "#FC931F" , "#058264"];
+
+      function getRandomNumber () {
+       return Math.floor(Math.random() * arr.length);
+      }
+
+      $(document).ready(function(){
+        $("path").click(function(){
+         const randIndex = getRandomNumber()
+         $(this).attr("fill", (arr[randIndex]))
+       })
+       return arr["randIndex"]
+      });
